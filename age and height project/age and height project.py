9e28177865c30c6bd1ec3f1ed318a874c99799a0
khaleb001 <129975955+khaleb001@users.noreply.github.com>
2023-07-09 18:09:10 +0100

    Add files via upload

---
+++ age and height project/age and height project.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in cm: "))
+
+if age >= 18 and height >= 170:
+    print("Welcome! You meet the age and height requirements.")
+else:
+    print("Sorry, you do not meet the age and/or height requirements.")
