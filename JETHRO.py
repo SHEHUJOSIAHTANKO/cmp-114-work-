d1fea4e22c69b0e7f0e13c2626d459a8f3b0dc65
Mwantok Kwoopnaan <130243517+Jethromwantok@users.noreply.github.com>
2023-07-12 15:26:02 +0100

    Add files via upload

---
+++ JETHRO.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in centimeters: "))
+
+if age >= 18 and height >= 180:
+    print("Welcome! You meet the necessary age and height requirements.")
+else:
+    print("Sorry, you do not meet the necessary age and/or height requirements.")
