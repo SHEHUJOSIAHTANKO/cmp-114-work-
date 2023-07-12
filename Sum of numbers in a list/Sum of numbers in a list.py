07981bae9b8313f2310a99b1876b9d11043a840c
Tonzy1 <130974534+Tonzy1@users.noreply.github.com>
2023-07-12 11:24:13 +0100

    Add files via upload

---
+++ Sum of numbers in a list/Sum of numbers in a list.py
@@ -0,0 +1,8 @@
+def calculate_sum(numbers):
+    total = sum(numbers)
+    return total
+
+# Example usage
+my_list = [1, 3, 4, 6, 7]
+result = calculate_sum(my_list)
+print("The sum of the numbers in the list is:", result)
